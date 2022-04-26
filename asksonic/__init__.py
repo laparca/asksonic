b6b53ef156a4377a85a9da192b01fc1551a9a081
Samuel R. Sevilla <laparca@laparca.es>
2022-04-26 13:44:12 +0000

    Added lang selection

--- asksonic/__init__.py
+++ asksonic/__init__.py
@@ -11,9 +11,10 @@ if getenv('FLASK_ENV') == 'development':
 
 route_prefix = getenv('ASKS_ROUTE_PREFIX', '/alexa')
 tracks_count = int(getenv('ASKS_TRACKS_COUNT', 50))
+lang = getenv('ASK_LANG', 'en')
 
 app = Flask(__name__)
-ask = Ask(app, route_prefix, path='templates/en.yaml')
+ask = Ask(app, route_prefix, path='templates/{}.yaml'.format(lang))
 
 logger = app.logger
 
