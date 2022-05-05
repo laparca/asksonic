3801b061c72bbfa0fcb621fbda715e0dd8c9d5a4
Samuel R. Sevilla <laparca@laparca.es>
2022-05-05 12:56:50 +0000

    feat: hability to disable verification of Alexa
    This could be good to do unit testing in the future

--- asksonic/__init__.py
+++ asksonic/__init__.py
@@ -12,8 +12,10 @@ if getenv('FLASK_ENV') == 'development':
 route_prefix = getenv('ASKS_ROUTE_PREFIX', '/alexa')
 tracks_count = int(getenv('ASKS_TRACKS_COUNT', 50))
 lang = getenv('ASKS_LANG', 'en')
+verify_requests = getenv('ASKS_VERIFY_REQUESTS', True)
 
 app = Flask(__name__)
+app.config['ASK_VERIFY_REQUESTS'] = verify_requests
 ask = Ask(app, route_prefix, path='templates/{}.yaml'.format(lang))
 
 logger = app.logger
